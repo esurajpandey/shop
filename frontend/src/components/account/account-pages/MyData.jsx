a4d8ae6bc474f6d37b3f9895874eef0e4889f9d2
esurajpandey <esurajpandey@gmail.com>
2024-03-05 20:39:58 +0530

    Changes seperate directory for app

--- frontend/src/components/account/account-pages/MyData.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyData = () => {
-  return <div>My Data</div>;
-};
-
-export default MyData;
--- frontend/src/utils/ToastMessage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ToastMessage = () => {
-  return <div></div>;
-};
-
-export default ToastMessage;
